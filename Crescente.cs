d153ccd1e7a5992d616f7cc861e77593a061053a
HDluid <luid.siul@gmail.com>
2024-08-02 13:21:11 +0100

    Crescente

---
+++ Crescente.cs
@@ -0,0 +1,29 @@
+﻿namespace Crescente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+
+            Console.WriteLine("  Digite dois números: ");
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+
+            while (n1 != n2)
+            {
+                if (n1 < n2)
+                {
+                    Console.WriteLine("  Crescente!");
+                }
+                else
+                {
+                    Console.WriteLine("  Decrescente!");
+                }
+                Console.WriteLine("  Digite outros dois números: ");
+                n1 = int.Parse(Console.ReadLine());
+                n2 = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
