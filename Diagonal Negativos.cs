950df7d1a7d1be62d2c1b85accfa9ed54b5408fc
HDluid <luid.siul@gmail.com>
2024-08-02 19:32:39 +0100

    Diagonal principal

---
+++ Diagonal Negativos.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DimagonalNegativos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, n, M, N;
+
+            Console.Write("  Qual é a ordem da matriz? ");
+            N = int.Parse(Console.ReadLine());
+            M = N;
+            int[,] mat = new int[M, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("  Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("  Diagonal Pricipal: ");
+            nn = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        nn = nn + 1;
+                    }
+                }
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+            Console.Write("  Quantidade de negativos: "+nn);
+        }
+    }
+}
